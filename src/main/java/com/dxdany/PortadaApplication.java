7345c832b046f23206916675bdbe08db1f14d29b
TechnologyLand <TechnologyLand@Danny>
2022-05-13 20:34:27 -0500

    Subida

---
+++ src/main/java/com/dxdany/PortadaApplication.java
@@ -0,0 +1,13 @@
+package com.dxdany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxdany/PortadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxdany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
